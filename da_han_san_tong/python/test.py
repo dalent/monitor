61a3ba88f212f55ae8c60a7901dc90ecebdbac78
jintao.ju <anqiu1987@sina.com>
2014-08-19 10:43:40 +0800

    Update test.py

--- da_han_san_tong/python/test.py
+++ da_han_san_tong/python/test.py
@@ -3,14 +3,14 @@ import requests
 import urllib2
 #payload = {'Account': 'dh20891', 'Pa\
 #ssword': 'daffacc5d5b2482ab603715a6d68566\
-#d', 'Phones': '1502693440\
+#d', 'Phones': '123456789\
 #9', 'Content': u"1.2.4.8的外网异常，已重置该接口，请密切关注".encode('utf-8')}
 #headers = {'Content-type': 'charset=utf-8'}
 payload = {'message':
            """<?xml version='1.0' encoding='UTF-8'?>
            <message><account>dh20891</account>
            <password>1231234</password>
-           <msgid></msgid><phones>1502634409</phones>
+           <msgid></msgid><phones>123456789</phones>
            <content>1.2.4.8的外网异常</content>
            <sign></sign><subcode></subcode><sendtime></sendtime></message>
            """}
